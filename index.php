310df41c9ebdc853d9880a7ee9fd0bb45565da28
Dhenmark Arquiza <dhenmark.arquiza@tatersgroup.com>
2021-03-13 15:00:41 +0000

    Update folder stracturing

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
         echo "Handler captured error $number: '$message'" . PHP_EOL  ;
     });
     
-    // Execute routing and class
+    // Execute route and class
     try {
         spl_autoload_register(function ($class_name) {
-            if(file_exists('./classes/'.$class_name.'.php')){
-                require_once './classes/'.$class_name.'.php';
-            } else if (file_exists('./Controller/'.$class_name.'.php')){
-                require_once './Controller/'.$class_name.'.php';
+            if(file_exists('./app/classes/'.$class_name.'.php')){
+                require_once './app/classes/'.$class_name.'.php';
+            } else if (file_exists('./app/controllers/'.$class_name.'.php')){
+                require_once './app/controllers/'.$class_name.'.php';
             }
         });
         
-        require_once('Routes.php');
+        require_once('./app/routes/Routes.php');
 
     } catch (Throwable $e) {
         echo "Captured Throwable: " . $e->getMessage() . PHP_EOL;
