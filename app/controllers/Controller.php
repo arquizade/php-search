d8b708925f5eea12378cb6579a6e1a02687675d0
Dhenmark Arquiza <dhenmark.arquiza@tatersgroup.com>
2021-03-13 18:51:32 +0000

    change folder location for controller-class

--- app/controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    class Controller extends Database {
-
-        public static function View($view_name){
-            include_once('./app/views/'.$view_name.'.php');
-        }
-    }
-?>
