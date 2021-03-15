de8d14c23331b53bce78068aa4ac47dd2e671a4a
Dhenmark Arquiza <dhenmark.arquiza@tatersgroup.com>
2021-03-15 00:12:33 +0000

    Update query for getting properties lessen time execution

--- app/controllers/Location.php
+++ app/controllers/Location.php
@@ -26,28 +26,23 @@ class Location extends Controller {
         static::model('location_model');
         $no_of_records_per_page = 2;
         $location =  $_SESSION['location_id'];
+        $sdate = $_SESSION['checkin'];
+        $edate = $_SESSION['checkout'];
         $page = ($_GET['page'] - 1) * $no_of_records_per_page;
-        $property_list = location_model::fetch_properties($location,$page,$no_of_records_per_page);
+        $property_list = location_model::fetch_properties($location,$sdate,$edate,$page,$no_of_records_per_page);
+
+        // print_r($property_list); exit();
 
         $tbody = array(); $result_count = 0;
         if(!empty($property_list)){
             $result_count = ceil(location_model::get_total_properties($location)[0][0] / $no_of_records_per_page);
-            $sdate = $_SESSION['checkin'];
-            $edate = $_SESSION['checkout'];
 
             $duplicate = array();
             foreach ($property_list as $value) {
-                
-                if(!empty($value['start_date'])){
-                    $stat = 'Available';
-                    if(($sdate >= $value['start_date']) && ($sdate <= $value['end_date'])){
-                        $stat = 'Not Available';
-                    }
-                    if(($edate >= $value['start_date']) && ($edate <= $value['end_date'])){
-                        $stat = 'Not Available';
-                    }
-                }else{
-                    $stat = 'Available';
+
+                $status = "Available";
+                if($value['status'] == 1){
+                    $status = "Not Available";
                 }
 
                 $pet_stat = "&#10004;";
@@ -60,23 +55,15 @@ class Location extends Controller {
                     $beach_stat = "&#x2716;";
                 }
 
-                $key = array_search($value['id'],$duplicate);
-
-                if($key === FALSE){
-                    $duplicate[] = $value['id'];
-                    $tbody[] = array(
-                        'uid'       => $value['id'],
-                        'name'      => $value['property_name'],
-                        'pet_stat'  => $pet_stat,
-                        'beach_stat'=> $beach_stat,
-                        'sleeps'    => $value['sleeps'],
-                        'beds'      => $value['beds'],
-                        'status'    => $stat
-                    );
-                }else{
-                    if($stat == 'Not Available')
-                        $tbody[$key]['status'] = $stat;
-                }
+                $tbody[] = array(
+                    'uid'       => $value['id'],
+                    'name'      => $value['property_name'],
+                    'pet_stat'  => $pet_stat,
+                    'beach_stat'=> $beach_stat,
+                    'sleeps'    => $value['sleeps'],
+                    'beds'      => $value['beds'],
+                    'status'    => $status
+                );
             }
         }
         $data['tbody'] = $tbody;
